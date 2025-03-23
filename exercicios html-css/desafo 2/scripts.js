3bf240f855f4c04d97e07bdaa8769cb400246159
LUCAS STEPHANO S FARFAN <lucasstephano85@gmail.com>
2025-03-23 14:01:25 -0300

    yk
    yk

--- exercicios html-css/desafo 2/scripts.js
+++ exercicios html-css/desafo 2/scripts.js
@@ -3,21 +3,28 @@ let depoisButton = document.getElementById('depois')
 let container = document.querySelector('.container')
 let items = document.querySelector('.item')
 let indicator = document.querySelector('indicador')
-let itemOld = document.querySelector('.active')
+let itemOld = document.querySelector('.lista .active')
 
 let active = 0
 let firstPosition = 0
-let lastPosition
+let lastPosition = item.length - 1
 
 
 depoisButton.onclick = () => {
 
+    let itemOld = document.querySelector('.lista .active')
     itemOld.classList.remove('active')
 
     active = active + 1 > lastPosition ? 0 : active + 1
-    items[active].classList.add('active')
+    item[active].classList.add('active')
+
+
 }
 
 antesButton.onclick = () => {
-    console.log("botao antes")
+    let itemOld = document.querySelector('.lista .active')
+    itemOld.classList.remove('active')
+
+    active = active - 1 < firstPosition ? lastPosition : active - 1
+    item[active].classList.add('active')
 }
